8da5cfc373acaa69e19ee0cee8de310e008675f0
Utku Balcıoğlu <utkumelih09@gmail.com>
2026-06-25 22:51:37 +0300

    Fix: Revert transpilePackages back to serverExternalPackages and add tracing for postman-request

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["iyzipay", "postman-request"],
   outputFileTracingIncludes: {
-    '/api/checkout': ['./node_modules/iyzipay/**/*'],
-    '/api/checkout/callback': ['./node_modules/iyzipay/**/*'],
-    '/api/**/*': ['./node_modules/iyzipay/**/*'],
+    '/api/checkout': ['./node_modules/iyzipay/**/*', './node_modules/postman-request/**/*'],
+    '/api/checkout/callback': ['./node_modules/iyzipay/**/*', './node_modules/postman-request/**/*'],
+    '/api/**/*': ['./node_modules/iyzipay/**/*', './node_modules/postman-request/**/*'],
   },
 };
 
